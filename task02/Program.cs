84b22ce283227a99e390b7600f9ba51e2ad10a10
F4RRIER <108473368+F4RRIER@users.noreply.github.com>
2022-10-07 11:22:26 +0300

    HW5

---
+++ task02/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write($"Введи количество элементов массива: ");
+int ElementsNumber = Convert.ToInt32(Console.ReadLine()); 
+
+int RandomNumbers(int ElementsNumber, int min, int max)
+  {
+  int[] randomNumbers = new int[ElementsNumber];
+  int sumElements = 0;
+  Console.Write("Получившийся массив: ");
+
+    for (int i = 0; i <randomNumbers.Length; i++ ){
+      randomNumbers[i] = new Random().Next(min, max);
+
+      Console.Write(randomNumbers[i] + " ");
+
+      if (i % 2 != 1)
+      {
+        sumElements = sumElements + randomNumbers[i];
+      }
+    }
+  return sumElements;
+  }
+
+int randomNumbers =  RandomNumbers(ElementsNumber, 1, 10);
+
+Console.WriteLine($"\nСумма элементов, стоящих на нечётных позициях: {randomNumbers}");
